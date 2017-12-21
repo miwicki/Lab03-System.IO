fbdaad25791c270019362b36f52ebc2d1e3dc0a1
Matt <wrestler.411@comcast.net>
2017-12-21 10:43:49 -0800

    Making main menu screen

--- word_guessing_game/word_guessing_game/Program.cs
+++ word_guessing_game/word_guessing_game/Program.cs
@@ -13,6 +13,44 @@ namespace word_guessing_game
                 string fileName = "words-file.txt";
                 string path = Path.Combine(Environment.CurrentDirectory, @"Words\", fileName);
             }
+            catch (DirectoryNotFoundException)
+            {
+
+                Console.WriteLine("Sike, that's the wrong directory!");
+            }
         }
-    }
+        static void Game(string path)
+        {
+            Init(path);
+            Console.Clear();
+            Console.WriteLine("Welcome to Matt Iwicki's Word Guessing Game!");
+            Console.WriteLine("Please choose one of the options below! To select an option, type its number and press enter");
+            Console.WriteLine("1. START GAME");
+            Console.WriteLine("2. VIEW ALL WORDS");
+            Console.WriteLine("3. ADD A WORD");
+            Console.WriteLine("4. DELETE A WORD");
+            Console.WriteLine("5. EXIT");
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+
+            }
+            if (userInput == "2")
+            {
+
+            }
+            if (userInput == "3")
+            {
+
+            }
+            if (userInput == "4")
+            {
+
+            }
+            if (userInput == "5")
+            {
+
+            }
+        }
+     }
 }
