861cb3ce4fedd2ac210fd3d0c644187090efe59e
Matt <wrestler.411@comcast.net>
2017-12-21 09:58:43 -0800

    Made text file, staring filepath

---
+++ word_guessing_game/word_guessing_game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace word_guessing_game
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string fileName = "words-file.txt";
+                string path = Path.Combine(Environment.CurrentDirectory, @"Words\", fileName);
+            }
+        }
+    }
+}
